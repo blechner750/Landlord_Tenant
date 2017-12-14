312ee85bb492bc4214b2fa24aadaa345fad46ac4
Brett Lechner <blechner750@gmail.com>
2017-12-14 17:33:23 -0500

    Merge

--- app/src/main/java/com/example/brett/landlord_tenant/TenantMainActivity.java
+++ app/src/main/java/com/example/brett/landlord_tenant/TenantMainActivity.java
@@ -80,7 +80,9 @@ public class TenantMainActivity extends AppCompatActivity {
     }
 
     public void rent(View view){
-        Intent intent = new Intent(this, rentActivity.class);
+        Intent intent = new Intent(this, LandlordRentActivity.class);
+        intent.putExtra("landlordUsername", name);
+        intent.putExtra("identifier", identifier);
         startActivity(intent);
     }
 
